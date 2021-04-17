78539805e1bc20c9538f6562117fdeeceb8c836d
Daniel Yamamoto <danylarayamamoto@hotmail.com>
2021-04-17 12:29:41 -0500

    setup's project

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, {StrictMode} from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+import "./assets/styles/normalize.css";
+import "./assets/sass/main.scss";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+const app = (
+    <StrictMode>
+        <App />
+    </StrictMode>
 );
+
+ReactDOM.render(app, document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
