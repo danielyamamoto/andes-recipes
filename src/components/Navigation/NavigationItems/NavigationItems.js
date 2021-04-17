355dcb2bfd6f871270e655ae54726f1faf27b201
Daniel Yamamoto <danylarayamamoto@hotmail.com>
2021-04-17 17:27:53 -0500

    Nav almost done

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -7,10 +7,18 @@ import classes from './NavigationItems.module.scss';
 import svgLogo from '../../../assets/img/logo.svg';
 import svgAdd from '../../../assets/img/add.svg';
 
-
 const navigationItems = props => (
-    <ul className={classes.navigationItems}>
-        <NavigationItem link="/" exact><Logo svgLogo={svgLogo} svgText="SVG-Logo principal" /></NavigationItem>
+    <ul className={[classes.navigationItems, classes.flexContainer, classes.flexContainer__justifyBetween, classes.flexContainer__itemsCenter].join(' ')}>
+        <div className={[classes.flexContainer, classes.flexContainer__justifyBetween, classes.flexContainer__itemsCenter].join(' ')}>
+            <div className={classes.addLogo}>
+                <Logo svgLogo={svgAdd} svgText="SVG-Add agrega nueva receta" />
+            </div>
+            <NavigationItem link="/" exact>ADD RECIPE</NavigationItem>
+        </div>
+        
+        <div>
+            <NavigationItem link="/" exact><Logo svgLogo={svgLogo} svgText="SVG-Logo principal" /></NavigationItem>
+        </div>
     </ul>
 );
 
--- src/components/Toolbar/Toolbar.js
+++ src/components/Toolbar/Toolbar.js
@@ -1,20 +1,12 @@
 import React from "react";
 
 import classes from "./Toolbar.module.scss";
-import svgAdd from "../../assets/img/add.svg";
 
-import Logo from "../Logo/Logo";
 import NavigationItems from "../Navigation/NavigationItems/NavigationItems";
 
 const toolbar = props => (
     <header className={classes.toolbar}>
-        <div className={[classes.flexContainer, classes.flexContainer__justifyBetween, classes.flexContainer__itemsCenter].join(' ')}>
-            <div className={classes.addLogo}>
-                <Logo svgLogo={svgAdd} svgText="SVG-Add agrega nueva receta" />
-            </div>
-            <p>ADD RECIPE</p>
-        </div>
-        <nav className={[classes.flexContainer, classes.flexContainer__justifyBetween, classes.flexContainer__itemsCenter].join(' ')}>
+        <nav>
             <NavigationItems />
         </nav>
     </header>
